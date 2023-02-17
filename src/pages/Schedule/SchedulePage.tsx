d813204c4f041ea46e0a6b15f7a34c876259a67d
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-02-17 14:39:29 +0700

    update

--- src/pages/Schedule/SchedulePage.tsx
+++ src/pages/Schedule/SchedulePage.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 
 export const SchedulePage: React.FC = () => {
+  const getSchedule = async (): Promise<any> => {};
+
   useEffect(() => {}, []);
 
   return <div>SchedulePage</div>;
