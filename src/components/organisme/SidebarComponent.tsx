cae9cf71e84a4c4d71577862614f1a5297702f73
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-02-20 10:12:00 +0700

    update

--- src/components/organisme/SidebarComponent.tsx
+++ src/components/organisme/SidebarComponent.tsx
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import SegmentIcon from "@mui/icons-material/Segment";
 import AssignmentOutlinedIcon from "@mui/icons-material/AssignmentOutlined";
 import AssessmentOutlinedIcon from "@mui/icons-material/AssessmentOutlined";
+import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
 import { Link } from "react-router-dom";
 
 const SidebarComponent: React.FC = () => {
   const menus = [
-    { name: "dashboard", link: "/", icon: SegmentIcon },
+    { name: "dashboard", link: "/", icon: DashboardOutlinedIcon },
     { name: "schedule", link: "/schedule", icon: AssignmentOutlinedIcon },
     { name: "report", link: "/report", icon: AssessmentOutlinedIcon },
   ];
@@ -28,12 +29,28 @@ const SidebarComponent: React.FC = () => {
             <Link
               to={menu.link}
               key={id}
-              className="flex items-center text-medium gap-3.5 p-2 hover:bg-gray-800 rounded-md"
+              className="group flex items-center text-medium gap-3.5 p-2 hover:bg-gray-800 rounded-md"
             >
               <div>
                 {React.createElement(menu.icon, { className: "text-sm" })}
               </div>
-              <h2 className={`whitespace-pre duration-500 ${!open && 'opacity-0 translate-x-28 overflow-hidden'}`}>{menu.name}</h2>
+              <h2
+                style={{
+                  transitionDelay: `${id + 3}00ms`,
+                }}
+                className={`whitespace-pre duration-500 ${
+                  !open && "opacity-0 translate-x-28 overflow-hidden"
+                }`}
+              >
+                {menu.name}
+              </h2>
+              <h2
+                className={`${
+                  open && `hidden`
+                } absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300`}
+              >
+                {menu.name}
+              </h2>
             </Link>
           ))}
         </div>
