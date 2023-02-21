9c6ed2ac062a90d826d76d9e71df30924304fcc4
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-02-21 11:18:17 +0700

    updae

--- src/components/moleculs/SeacrhHeaderComponent.tsx
+++ src/components/moleculs/SeacrhHeaderComponent.tsx
@@ -1,15 +1,35 @@
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import ShortcutOutlinedIcon from "@mui/icons-material/ShortcutOutlined";
+import { useState, useRef, useEffect } from "react";
 
 const SeacrhHeaderComponent: React.FC = () => {
+  const [active, setActive] = useState<boolean>(false);
+  const [onSearch, setOnsearch] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const click = (): void => {
+    setActive(!active);
+    active ? inputRef.current?.blur() : inputRef.current?.focus();
+  };
+
   return (
     <div className="w-72 relative">
-      <div className="border w-full  h-9 rounded-md bg-gray-100 border-gray-200 flex items-center">
+      <div
+        className="border w-full  h-9 rounded-md bg-gray-100 border-gray-200 flex items-center"
+        onClick={click}
+      >
         <SearchOutlinedIcon
           style={{ fontSize: 20 }}
           className="text-gray-400 ml-2 mt-1"
         />
         <input
+          onBlur={() => {
+            if (active && !onSearch) {
+              setActive(false);
+              inputRef.current?.blur();
+            }
+          }}
+          ref={inputRef}
           placeholder="Seacrh a menu"
           className="bg-gray-100 text-[0.8em] font-sans font-medium px-1 flex-1 mr-2 outline-none"
         />
@@ -18,7 +38,21 @@ const SeacrhHeaderComponent: React.FC = () => {
           <h6 className="text-[0.8em] font-medium">Enter</h6>
         </div>
       </div>
-      <ul className="w-full border h-52 absolute z-10 top-8 bg-white rounded-b-md drop-shadow-sm "></ul>
+      <ul
+        onClick={() => {
+          setActive(false);
+          inputRef.current?.blur();
+        }}
+        onMouseLeave={() => setOnsearch(false)}
+        onMouseEnter={() => setOnsearch(true)}
+        className={`${
+          !active && "hidden duration-500 "
+        } w-full border h-52 absolute z-10 top-8 bg-white rounded-b-md drop-shadow-sm `}
+      >
+        <li className="" onClick={() => alert("halow")}>
+          Form schedule
+        </li>
+      </ul>
     </div>
   );
 };
