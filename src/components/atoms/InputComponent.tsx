9fa91bf46379b4483b45ce009dfd3b0d60cb687d
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-02-27 14:30:40 +0700

    upate

--- src/components/atoms/InputComponent.tsx
+++ src/components/atoms/InputComponent.tsx
@@ -4,22 +4,31 @@ interface IProps {
   className?: React.HTMLAttributes<HTMLDivElement> | string | undefined;
   mandatoy?: boolean;
   disabled?: boolean;
+  label?: String;
   // value: String | number;
   // setValue(): Promise<any> | any;
 }
 
-const InputComponent: React.FC<IProps> = ({ className }) => {
+const InputComponent: React.FC<IProps> = ({
+  className,
+  mandatoy,
+  disabled,
+  label,
+}) => {
   const inputRef = useRef();
   const modalRef = useRef();
 
   useEffect(() => {}, []);
 
   return (
-    <>
-      <div className={`w-full   rounded-md bg-gray-50 ${className}`}>
-        <input className=" w-full font-normal border h-7 rounded-md bg-gray-50 outline-none px-3" />
-      </div>
-    </>
+    <div className={`w-full   rounded-md bg-gray-50 ${className}`}>
+      {label && <label>{label}</label>}
+      <input
+        className={`${
+          label && "mt-1"
+        } w-full font-normal border h-7 rounded-md bg-gray-50 outline-none px-3`}
+      />
+    </div>
   );
 };
 
