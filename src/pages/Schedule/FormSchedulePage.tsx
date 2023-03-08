020d21a6818e2b930a98522afe96393a0322fe67
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-03-08 15:52:58 +0700

    upate

--- src/pages/Schedule/FormSchedulePage.tsx
+++ src/pages/Schedule/FormSchedulePage.tsx
@@ -123,145 +123,179 @@ const FormSchedulePage: React.FC = () => {
   console.log(scroll);
 
   return (
-    <div
-      className="  max-h-[calc(100vh-70px)] overflow-y-auto scrollbar-thin scrollbar-track-gray-200 scrollbar-thumb-gray-300"
-      onScroll={(e: any) => setScroll(e.target.scrollTop)}
-    >
-      {!loading ? (
-        <>
-          <div
-            className={`w-full flex  justify-between px-5 ${
-              scroll > 0
-                ? "bg-white border-b border-gray-100 drop-shadow-sm"
-                : "bg-gray-100"
-            } py-6 sticky top-0 z-[51] duration-100 `}
-          >
-            <div className="flex  items-center">
-              <h4
-                onClick={() => navigate("/schedule")}
-                className="font-bold text-lg mr-2 cursor-pointer"
-              >
-                New Schedule
-              </h4>
-              <div className="text-[0.9em]">
-                <ButtonStatusComponent
-                  // className="text-[0.7em]"
-                  status={data.status ?? "0"}
-                  name={data.workflowState ?? "Not Save"}
+    <>
+      <div
+        className="  max-h-[calc(100vh-70px)] overflow-y-auto scrollbar-thin scrollbar-track-gray-200 scrollbar-thumb-gray-300"
+        onScroll={(e: any) => setScroll(e.target.scrollTop)}
+      >
+        {!loading ? (
+          <>
+            <div
+              className={`w-full flex  justify-between px-5 ${
+                scroll > 0
+                  ? "bg-white border-b border-gray-100 drop-shadow-sm"
+                  : "bg-gray-100"
+              } py-6 sticky top-0 z-[51] duration-100 `}
+            >
+              <div className="flex  items-center">
+                <h4
+                  onClick={() => navigate("/schedule")}
+                  className="font-bold text-lg mr-2 cursor-pointer"
+                >
+                  New Schedule
+                </h4>
+                <div className="text-[0.9em]">
+                  <ButtonStatusComponent
+                    // className="text-[0.7em]"
+                    status={data.status ?? "0"}
+                    name={data.workflowState ?? "Not Save"}
+                  />
+                </div>
+              </div>
+              <div className="flex">
+                <IconButton
+                  classModal="top-[28px]"
+                  primary
+                  Icon={MoreHorizIcon}
+                  iconSize={15}
+                  classIcon="mt-1"
+                  list={[
+                    { name: "Print", onClick: () => {} },
+                    { name: "Duplicate", onClick: () => {} },
+                    { name: "Export PDF", onClick: () => {} },
+                  ]}
+                  iconListDisabled
+                  className={` duration-100 mr-2 px-2 `}
+                />
+                <IconButton
+                  name="Save"
+                  callback={onSave}
+                  className={`opacity-80 hover:opacity-100 duration-100  `}
                 />
               </div>
             </div>
-            <div className="flex">
-              <IconButton
-                classModal="top-[28px]"
-                primary
-                Icon={MoreHorizIcon}
-                iconSize={15}
-                classIcon="mt-1"
-                list={[
-                  { name: "Print", onClick: () => {} },
-                  { name: "Duplicate", onClick: () => {} },
-                  { name: "Export PDF", onClick: () => {} },
-                ]}
-                iconListDisabled
-                className={` duration-100 mr-2 px-2 `}
-              />
-              <IconButton
-                name="Save"
-                callback={onSave}
-                className={`opacity-80 hover:opacity-100 duration-100  `}
-              />
-            </div>
-          </div>
-          <div className=" px-5 flex flex-col ">
-            <div className="border w-full flex-1  bg-white rounded-md overflow-y-scroll scrollbar-none">
-              <div className="w-full h-auto  float-left rounded-md p-3 py-5">
-                <div className=" w-1/2 px-4 float-left ">
-                  <InputComponent
-                    label="Name"
-                    value={name}
-                    className="h-[38px] text-[0.93em] mb-3"
-                    onChange={(e) =>
-                      setName({
-                        valueData: e,
-                        valueInput: e,
-                      })
-                    }
-                  />
-                  <InputComponent
-                    label="Warehouse"
-                    value={warehouse}
-                    className="h-[38px]   text-[0.93em] mb-3"
-                    onChange={(e) =>
-                      setWarehouse({
-                        valueData: e,
-                        valueInput: e,
-                      })
-                    }
-                    mandatoy
-                  />
-                  <InputComponent
-                    label="User"
-                    value={user}
-                    className="h-[38px]   text-[0.93em] mb-3"
-                    onChange={(e) =>
-                      setUser({
-                        valueData: e,
-                        valueInput: e,
-                      })
-                    }
-                    disabled
-                  />
-                </div>
-                <div className=" w-1/2 px-4 float-left  mb-3">
-                  <InputComponent
-                    label="Date"
-                    value={createdAt}
-                    className="h-[38px]  text-[0.93em] mb-3"
-                    type="date"
-                    onChange={(e) =>
-                      setCreatedAt({
-                        valueData: e,
-                        valueInput: e,
-                      })
-                    }
-                    disabled
-                  />
-                  <InputComponent
-                    label="startDate"
-                    value={startDate}
-                    className="h-[38px]  text-[0.93em] mb-3"
-                    type="date"
-                    onChange={(e) =>
-                      setStartDate({
-                        valueData: e,
-                        valueInput: e,
-                      })
-                    }
-                    mandatoy
-                  />
-                  <InputComponent
-                    label="dueDate"
-                    value={dueDate}
-                    className="h-[38px]  text-[0.93em] mb-3"
-                    type="date"
-                    onChange={(e) =>
-                      setDueDate({
-                        valueData: e,
-                        valueInput: e,
-                      })
-                    }
-                    mandatoy
-                  />
+            <div className=" px-5 flex flex-col ">
+              <div className="border w-full flex-1  bg-white rounded-md overflow-y-scroll scrollbar-none">
+                <div className="w-full h-auto  float-left rounded-md p-3 py-5">
+                  <div className=" w-1/2 px-4 float-left ">
+                    <InputComponent
+                      label="Name"
+                      value={name}
+                      className="h-[38px] text-[0.93em] mb-3"
+                      onChange={(e) =>
+                        setName({
+                          valueData: e,
+                          valueInput: e,
+                        })
+                      }
+                    />
+                    <InputComponent
+                      label="Warehouse"
+                      value={warehouse}
+                      className="h-[38px]   text-[0.93em] mb-3"
+                      onChange={(e) =>
+                        setWarehouse({
+                          valueData: e,
+                          valueInput: e,
+                        })
+                      }
+                      mandatoy
+                    />
+                    <InputComponent
+                      label="User"
+                      value={user}
+                      className="h-[38px]   text-[0.93em] mb-3"
+                      onChange={(e) =>
+                        setUser({
+                          valueData: e,
+                          valueInput: e,
+                        })
+                      }
+                      disabled
+                    />
+                  </div>
+                  <div className=" w-1/2 px-4 float-left  mb-3">
+                    <InputComponent
+                      label="Date"
+                      value={createdAt}
+                      className="h-[38px]  text-[0.93em] mb-3"
+                      type="date"
+                      onChange={(e) =>
+                        setCreatedAt({
+                          valueData: e,
+                          valueInput: e,
+                        })
+                      }
+                      disabled
+                    />
+                    <InputComponent
+                      label="startDate"
+                      value={startDate}
+                      className="h-[38px]  text-[0.93em] mb-3"
+                      type="date"
+                      onChange={(e) =>
+                        setStartDate({
+                          valueData: e,
+                          valueInput: e,
+                        })
+                      }
+                      mandatoy
+                    />
+                    <InputComponent
+                      label="dueDate"
+                      value={dueDate}
+                      className="h-[38px]  text-[0.93em] mb-3"
+                      type="date"
+                      onChange={(e) =>
+                        setDueDate({
+                          valueData: e,
+                          valueInput: e,
+                        })
+                      }
+                      mandatoy
+                    />
+                  </div>
                 </div>
               </div>
+              <ul className="mt-10 mb-8 text-sm ml-12">
+                <li className=" pl-5 pb-6   border-l border-[#e4e6e5] relative float-left w-[60%] text-start">
+                  <div className="-mt-[7px] w-[15px] h-[15px] border border-gray-300 absolute rounded-full bg-white -left-[7px]">
+                    <div className="w-[6px] h-[6px] border absolute rounded-full bg-gray-600 top-[4.1px] left-[3.8px]"></div>
+                  </div>
+                  <h6 className="-mt-[10px] ml-3">
+                    <b className="font-medium">Muhammad Takiyudin</b> changed
+                    values for Available Qty at From Warehouse from 2.287 to
+                    2.237 in row #0, Available Qty at From Warehouse from 250 to
+                    247 in row #2 - 4 hours ago
+                  </h6>
+                </li>
+                <li className="mt-1 pl-5 pb-6   border-l   border-[#e4e6e5] relative float-left w-[60%] text-start">
+                  <div className="-mt-[7px] w-[15px] h-[15px] border border-gray-300 absolute rounded-full bg-white -left-[7px]">
+                    <div className="w-[6px] h-[6px] border absolute rounded-full bg-gray-600 top-[4.1px] left-[3.8px]"></div>
+                  </div>
+                  <h6 className="-mt-[10px] ml-3">
+                    <b className="font-medium">Afrianti Lestari</b> In
+                    Confirmation Doc - 6 hours ago
+                  </h6>
+                </li>
+                <li className="mt-1 pl-5 pb-6   border-[#e4e6e5] relative float-left w-[60%] text-start">
+                  <div className="-mt-[7px] w-[15px] h-[15px] border border-gray-300 absolute rounded-full bg-white -left-[7px]">
+                    <div className="w-[6px] h-[6px] border absolute rounded-full bg-gray-600 top-[4.1px] left-[3.8px]"></div>
+                  </div>
+                  <h6 className="-mt-[10px] ml-3">
+                    <b className="font-medium">Afrianti Lestari</b> In changed
+                    value of Workflow State from Draft to In Confirmation Doc -
+                    6 hours ago
+                  </h6>
+                </li>
+              </ul>
             </div>
-          </div>
-        </>
-      ) : (
-        <LoadingComponent />
-      )}
-    </div>
+          </>
+        ) : (
+          <LoadingComponent />
+        )}
+      </div>
+    </>
   );
 };
 
