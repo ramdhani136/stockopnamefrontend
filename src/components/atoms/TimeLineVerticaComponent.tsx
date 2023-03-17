4fa03de6102e34ec41582e0e14977c336890010c
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-03-17 13:29:21 +0700

    upate

--- src/components/atoms/TimeLineVerticaComponent.tsx
+++ src/components/atoms/TimeLineVerticaComponent.tsx
@@ -1,10 +1,36 @@
 import moment from "moment";
+import { useState } from "react";
 
 interface IProps {
   data: any[];
 }
 
+interface ITime {
+  date: string;
+}
+
 const TimeLineVertical: React.FC<IProps> = ({ data }) => {
+  // Time Component
+  const TimeComponent: React.FC<ITime> = ({ date }) => {
+    const [open, setOpen] = useState<boolean>(false);
+    return (
+      <div
+        className="inline relative cursor-pointer"
+        onMouseEnter={() => setOpen(true)}
+        onMouseLeave={() => setOpen(false)}
+      >
+        <h6 className="inline">{moment(date).fromNow()}</h6>
+
+        {open && (
+          <h6 className="absolute left-0 -top-5 w-[170px] text-center text-[0.95em] p-1 py-[1px] bg-slate-900 text-white rounded-md">
+           {moment(`${date}`).format("lll")}
+          </h6>
+        )}
+      </div>
+    );
+  };
+  // End
+
   return (
     <ul className="mt-10 mb-8 text-sm ml-12">
       {data.length > 0 &&
@@ -18,10 +44,10 @@ const TimeLineVertical: React.FC<IProps> = ({ data }) => {
             <div className="-mt-[7px] w-[15px] h-[15px] border border-gray-300 absolute rounded-full bg-white -left-[7px]">
               <div className="w-[6px] h-[6px] border absolute rounded-full bg-gray-600 top-[4px] left-[3.8px]"></div>
             </div>
-            <h6 className="-mt-[10px] ml-3">
+            <div className="-mt-[10px] ml-3">
               <b className="font-medium">{item.user.name}</b> {item.message} -{" "}
-              {moment(item.createdAt).fromNow()}
-            </h6>
+              <TimeComponent date={item.createdAt} />
+            </div>
           </li>
         ))}
     </ul>
