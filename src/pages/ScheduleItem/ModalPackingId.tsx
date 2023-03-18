3431a5fafd0bd6ed6f6caa138caa3988e1e4f765
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-03-18 13:13:09 +0700

    upate

--- src/pages/ScheduleItem/ModalPackingId.tsx
+++ src/pages/ScheduleItem/ModalPackingId.tsx
@@ -4,7 +4,8 @@ import { IListInput, IValue } from "../../components/atoms/InputComponent";
 import GetDataServer, { DataAPI } from "../../utils/GetDataServer";
 
 const ModalPackingId: React.FC = () => {
-  const [data, setData] = useState<IListInput[]>([]);
+  const [allData, setAllData] = useState<IListInput[]>([]);
+  const [data, setData] = useState<any>({});
 
   const getData = async (): Promise<void> => {
     try {
@@ -18,7 +19,7 @@ const ModalPackingId: React.FC = () => {
             };
           }
         );
-        setData(genData);
+        setAllData(genData);
       }
     } catch (error) {
       console.log(error);
@@ -35,15 +36,59 @@ const ModalPackingId: React.FC = () => {
   }, []);
 
   return (
-    <div className=" w-[450px] h-[auto] max-h-[400px]scrollbar-thin scrollbar-track-gray-100 p-5 scrollbar-thumb-gray-200">
+    <div className=" w-[450px] h-[auto] max-h-[400px]scrollbar-thin scrollbar-track-gray-100 p-7 scrollbar-thumb-gray-200">
       <InputComponent
         value={packingId}
-        list={data}
+        list={allData}
         label="Packing ID"
         onChange={(e) => {
           setPackingId({ ...packingId, valueInput: e });
         }}
+        onSelected={(e) => {
+          setData(e.value),
+            setPackingId({
+              valueData: e.value.id_packing,
+              valueInput: e.value.id_packing,
+            });
+        }}
+        className="mb-2"
       />
+      {data.item && (
+        <InputComponent
+          value={{ valueData: data.item, valueInput: data.item }}
+          label="Item Code"
+          className="mb-2"
+        />
+      )}
+      {data.item_name && (
+        <InputComponent
+          value={{ valueData: data.item_name, valueInput: data.item_name }}
+          label="Item Name"
+          className="mb-2"
+        />
+      )}
+      {data.conversion && (
+        <InputComponent
+          value={{ valueData: data.conversion, valueInput: data.conversion }}
+          label="Qty"
+          className="mb-2"
+        />
+      )}
+      {data.conversion && (
+        <InputComponent
+          value={{ valueData: null, valueInput: "" }}
+          mandatoy
+          label="Actual Stock"
+          className="mb-2"
+        />
+      )}
+      {data.stock_uom && (
+        <InputComponent
+          value={{ valueData: data.stock_uom, valueInput: data.stock_uom }}
+          label="Uom"
+          className="mb-2"
+        />
+      )}
     </div>
   );
 };
