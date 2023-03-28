4977b86d940741fa91d8872cbf9e562c1ade3de8
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-03-28 14:16:09 +0700

    update

--- src/components/organisme/ChatsConversions.tsx
+++ src/components/organisme/ChatsConversions.tsx
@@ -147,7 +147,9 @@ const ChatsConversions: React.FC<IProps> = ({ setLoading }) => {
               <div className="ml-2 flex flex-col justify-center">
                 <b className="text-[0.9em]">{item.user.name}</b>
                 <h5 className="text-[0.8em] text-gray-500 -mt-[3px]">
-                  {item.latestMessage.content}
+                  {item.latestMessage.content.length > 10
+                    ? `${item.latestMessage.content.substring(0, 10)} ...`
+                    : item.latestMessage.content}
                 </h5>
               </div>
             </li>
